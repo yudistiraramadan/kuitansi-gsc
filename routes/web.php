3f61c7806c6407fc24d4073a19e20e7c935bef38
yudistiraramadan <yudistiraramadankalimasada@gmail.com>
2024-05-20 15:49:16 +0700

    create login page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'loginpage'])->name('loginpage');
 
 Route::get('/dashboard', function (){
     return view('dashboard', ['title'=> 'Dashboard']);
